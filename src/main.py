5a7296a9150933aafe2146a6fc4654fa5ff5441f
George <georgedittmar@gmail.com>
2025-03-02 12:35:16 -0800

    renaming functions

--- src/main.py
+++ src/main.py
@@ -9,17 +9,18 @@ async def root():
     return {"message": f"ArcaneArchive {VERSION}"}
 
 @app.get("/health")
-async def root():
+async def health_check():
     return {"message": f"ArcaneArchive {VERSION} \n Status: UP"}
 
 @app.post("/v1/document/ingest")
-async def root():
+async def document_ingestion():
     return {"message": f"ArcaneArchive {VERSION} \n Status: UP"}
 
 @app.post("/v1/document/update")
-async def root():
+async def document_update():
     return {"message": f"ArcaneArchive {VERSION} \n Status: UP"}
 
 @app.post("/v1/agents/invoke")
-async def root():
+async def agent_call():
+    """Handles a call to the Agents which will autonamously decide what to do with the user input."""
     return {"message": f"ArcaneArchive {VERSION} \n Status: UP"}
